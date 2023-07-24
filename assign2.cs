6a6dfc5c46a5a85652e282634a2bd1d27c80aa78
Jayesh4t5 <jayeshshinde8511@gmail.com>
2023-07-23 22:12:35 -0700

    Add project files.

---
+++ assign2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//wap to find out the digits which are divisible by 3
+
+namespace assignmentkmam
+{
+    public class assign2
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            int digit;
+            Console.WriteLine("Enter the Number:");
+            num=Convert.ToInt32(Console.ReadLine());
+
+            while (num>0) 
+            {
+                digit = num % 10;
+
+                if (digit%3 == 0)
+                {
+                    Console.WriteLine(digit);
+
+                }
+                num = num / 10;
+
+            }
+            
+
+        }
+    }
+}
---
+++ fistandlastsum.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace assignmentkmam
+//{
+//   public class Fistandlastsum
+//    {
+//        static void Main(string[] args)
+//        {
+//            int n;
+//            int FirstD;
+//            int lastD;
+//            int sum;
+//            Console.WriteLine("Enter the number:");
+//            n=Convert.ToInt32(Console.ReadLine());
+
+//             lastD = n % 10;
+//            FirstD = 0;
+//            n = n / 10;
+//             sum = 0;
+
+//            while (n > 0)
+//            {
+//                int cdigit=n%10;
+//                FirstD=cdigit;
+//                n = n / 10;
+
+//            }
+//            sum=FirstD+lastD;
+//            Console.WriteLine(sum);
+
+//        }
+//    }
+//}
